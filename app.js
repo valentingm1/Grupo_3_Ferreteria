5974d79063b003f8817372ee2675bd1e866cd83d
NeedAssistance <gomarchjjba@gmail.com>
2022-03-26 15:01:58 -0300

    carrito de compras en proceso

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const fs = require("fs")
+const app = express()
+app.use(express.static('public'))
+
+app.listen(3000,() =>{
+    console.log("Todo sobre ruedas")
+})
+app.get("/", (req,res)=>{
+    res.sendFile(__dirname+"/views/elcoso.html")
+})
