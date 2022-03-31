3df9bf87f11a26175c4172f84e581af50a1d2064
adrisa17 <100723839+adrisa17@users.noreply.github.com>
2022-03-31 14:05:43 -0500

    se unificó el archivo con el home, también se hicieron correcciones a los links

--- app.js
+++ app.js
@@ -7,5 +7,14 @@ app.listen(3000,() =>{
     console.log("Todo sobre ruedas")
 })
 app.get("/", (req,res)=>{
-    res.sendFile(__dirname+"/views/elcoso.html")
-})
+    res.sendFile(__dirname+"/views/index.html")
+});
+app.get("/productCart", (req,res)=>{
+    res.sendFile(__dirname+"/views/productCart.html")
+});
+app.get("/register", (req,res)=>{
+    res.sendFile(__dirname+"/views/register.html")
+});
+app.get("/productDetail", (req,res)=>{
+    res.sendFile(__dirname+"/views/productDetail.html")
+});
