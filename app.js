be820caaa57b6fad092028a2206b81ca3e86f3ea
ValentinGomarch <gomarchjjba@gmail.com>
2022-04-14 21:21:36 -0300

    Todo el proyecto hasta ahora 14/4

--- app.js
+++ app.js
@@ -7,5 +7,21 @@ app.listen(3000,() =>{
     console.log("Todo sobre ruedas")
 })
 app.get("/", (req,res)=>{
-    res.sendFile(__dirname+"/views/carrito.html")
+    res.sendFile(__dirname+"/views/index.html")
 })
+
+app.get("/register", (req,res)=>{
+    res.sendFile(__dirname+"/views/register.html")
+})
+
+app.get("/login", (req,res)=>{
+    res.sendFile(__dirname+"/views/login.html")
+})
+
+app.get('/detalle', (req,res)=>{
+    res.sendFile(__dirname + '/views/productDetail.html');
+});
+
+app.get('/carrito', (req,res)=>{
+    res.sendFile(__dirname + '/views/carrito.html');
+});
